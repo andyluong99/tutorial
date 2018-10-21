34218d24b1806a4374391ecbe0cc3890baa1be3e
Andy Luong <andyluong99@gmail.com>
2018-10-21 18:56:28 -0500

    hello world

--- tutorial/tutorial/tutorial.cs
+++ tutorial/tutorial/tutorial.cs
@@ -5,11 +5,18 @@ namespace Tutorial
     {
         static void Main()
         {
+            String yourName = "";
+
             Console.WriteLine("Hello World!\n");
 
             // Keep the console window open in debug mode.
             Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
+
+            Console.WriteLine("What is your name?");
+            yourName = Console.ReadLine();
+
+            Console.WriteLine("Your name is " + yourName);
         }
     }
 }
