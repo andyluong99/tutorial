4b8dec40fee533d86edc19c14cd42af4b55d6477
Andy Luong <andyluong99@gmail.com>
2018-10-21 18:22:55 -0500

    added tutorial

---
+++ tutorial/tutorial/tutorial.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Tutorial
+{
+    class Tutorial
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!\n");
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
